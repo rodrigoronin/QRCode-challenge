c4f801bb9babe499a7a4ba2259d4634642928da2
rodrigoronin <liramailbox@gmail.com>
2025-11-25 02:41:52 -0300

    feat: updated main.ts to use player class

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { Application, Assets, Container, Texture, Rectangle, Sprite } from "pixi.js";
-import { InputManager, InputAction } from "./core/input/InputManager";
+import { Application, Assets, Container, Texture, Rectangle } from "pixi.js";
+import { InputManager } from "./input/InputManager";
+import { Player } from "./entities/Player";
+
 import "./style.css";
 
 // Assets
@@ -10,13 +12,14 @@ import player_01 from "./assets/placeholder_player_01.png";
   await game.init({
     width: 1280,
     height: 720,
-    background: "#d2d2d2",
+    background: "#223222",
     // resizeTo: window,
   });
-  const input = new InputManager();
   document.body.appendChild(game.canvas);
 
+  const input: InputManager = new InputManager();
   const entities: Container = new Container();
+  game.stage.addChild(entities);
 
   const playerTexture = await Assets.load(player_01);
   playerTexture.source.style.magFilter = "nearest";
@@ -26,39 +29,12 @@ import player_01 from "./assets/placeholder_player_01.png";
     frame: new Rectangle(0, 0, 32, 32),
   });
 
-  const player: Sprite = new Sprite(frame);
-  player.anchor.set(0.5);
-  player.scale.set(2);
-  player.x = game.screen.width / 2;
-  player.y = game.screen.height / 2;
-
-  function updatePlayer(deltaTime: number): void {
-    const speed: number = 200;
-    let vx: number = 0;
-    let vy: number = 0;
-
-    if (input.isPressed(InputAction.Up)) vy -= 1;
-    if (input.isPressed(InputAction.Down)) vy += 1;
-    if (input.isPressed(InputAction.Left)) vx -= 1;
-    if (input.isPressed(InputAction.Right)) vx += 1;
-
-    const magnitude: number = Math.hypot(vx, vy);
-    if (magnitude > 0) {
-      vx = (vx / magnitude) * speed;
-      vy = (vy / magnitude) * speed;
-    }
-
-    player.x += (vx * deltaTime) / 1000;
-    player.y += (vy * deltaTime) / 1000;
-
-    console.log(player.x);
-  }
-
-  entities.addChild(player);
-
-  game.stage.addChild(entities);
+  const player: Player = new Player(frame, input);
+  player.container.x = game.screen.width / 2;
+  player.container.y = game.screen.height / 2;
+  player.addTo(entities);
 
   game.ticker.add((ticker) => {
-    updatePlayer(ticker.deltaMS);
+    player.update(ticker.deltaMS);
   });
 })();
