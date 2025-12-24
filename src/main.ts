48385e3559312bdb7256c0c05fa64852d5ac0545
rodrigoronin <liramailbox@gmail.com>
2025-12-24 19:33:16 -0300

    feat: enemy animations

--- src/main.ts
+++ src/main.ts
@@ -66,6 +66,10 @@ import map_01_colliders from "./assets/maps/mock_map_01.json";
     idle_left: frameSlicer(assets["_toxic_fly"], frameSize, 4, 0),
     idle_up: frameSlicer(assets["_toxic_fly"], frameSize, 4, 0),
     idle_right: frameSlicer(assets["_toxic_fly"], frameSize, 4, 0),
+    walk_down: frameSlicer(assets["_toxic_fly"], frameSize, 4, 0),
+    walk_right: frameSlicer(assets["_toxic_fly"], frameSize, 4, 0),
+    walk_up: frameSlicer(assets["_toxic_fly"], frameSize, 4, 1),
+    walk_left: frameSlicer(assets["_toxic_fly"], frameSize, 4, 1),
   };
 
   const player: Player = new Player(frames);
@@ -73,20 +77,15 @@ import map_01_colliders from "./assets/maps/mock_map_01.json";
   player.container.y = 512;
 
   // Enemy prototype
-  const enemySprite = new Sprite(enemyFrames["idle_down"][0]);
-  const enemy_01 = new Enemy(enemySprite, player);
-  enemy_01.container.x = 600;
+  const enemy_01 = new Enemy(enemyFrames, player);
+  enemy_01.container.x = 500;
   enemy_01.container.y = 512;
-
-  // const enemySprite2 = new Sprite(frames["dash_down"][4]);
-  // const enemy_02 = new Enemy(enemySprite2, player);
-  // enemy_02.container.x = 560;
-  // enemy_02.container.y = 582;
-
-  // const enemySprite3 = new Sprite(frames["dash_down"][4]);
-  // const enemy_03 = new Enemy(enemySprite3, player);
-  // enemy_03.container.x = 595;
-  // enemy_03.container.y = 582;
+  const enemy_02 = new Enemy(enemyFrames, player);
+  enemy_02.container.x = 500;
+  enemy_02.container.y = 612;
+  const enemy_03 = new Enemy(enemyFrames, player);
+  enemy_03.container.x = 580;
+  enemy_03.container.y = 512;
 
   const map: Sprite = new Sprite(
     new Texture({
@@ -106,10 +105,12 @@ import map_01_colliders from "./assets/maps/mock_map_01.json";
   camera.addChild(world);
   world.addChild(map);
   world.addChild(enemy_01.container);
+  world.addChild(enemy_02.container);
+  world.addChild(enemy_03.container);
   world.addChild(player.container);
   world.addChild(...walls.map((wall) => wall.container));
 
-  enemiesList.push(enemy_01);
+  enemiesList.push(enemy_01, enemy_02, enemy_03);
 
   resizeViewport(viewport);
 
